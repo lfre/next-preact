d0eeecd3af2b4635eb27125002f1c8818785d3d3
Alfredo Lopez <lfre@users.noreply.github.com>
2023-06-09 15:51:10 -0400

    feat: support next 13.4.4

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
+import { Inter } from 'next/font'
 import styles from '@/styles/Home.module.css'
 import dynamic from 'next/dynamic';
 
