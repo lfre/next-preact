68d76ea15fb84674d2d9ba4b5536269aa84a1a99
Alfredo Lopez <lfre@users.noreply.github.com>
2025-03-14 16:22:22 -0400

    feat: upgrade @prefresh/webpack and override entryOptions

--- next.config.js
+++ next.config.js
@@ -7,22 +7,32 @@ module.exports = require('next-plugin-preact')({
   webpack(config, options) {
     const { dev, isServer } = options;
     if (dev && !isServer) {
-      config.plugins.unshift({
-        apply(compiler) {
-          compiler.hooks.thisCompilation.tap('Prefresh', (compilation) => {
-            /*
-              Since v15.2.0-canary.56, Next added webpack layers in this PR:
-              https://github.com/vercel/next.js/pull/75878
-              Any plugin that adds additional entries needs to specify the right layer.
-              Otherwise, the entry will be considered a duplicate in the bundle.
-              This is mostly fine, except that for Prefresh to work, it needs to set options.vnode in the same module in memory.
-              Without a layer, the options.vnode gets set in a different object in memory than the preact module the app loads.
-            */
-            // !IMPORTANT: This changes every entry, this can be more specific when Prefresh allows overriding the individual entry option
-            compilation.globalEntry.options.layer = WEBPACK_LAYERS.pagesDirBrowser;
-          });
-        }
-      });
+      /*
+        Since v15.2.0-canary.56, Next added webpack layers in this PR:
+        https://github.com/vercel/next.js/pull/75878
+        Any plugin that adds additional entries needs to specify the right layer.
+        Otherwise, the entry will be considered a duplicate in the bundle.
+        This is mostly fine, except that for Prefresh to work, it needs to set options.vnode in the same module in memory.
+        Without a layer, the options.vnode gets set in a different object in memory than the preact module the app loads.
+      */
+      const prefreshPlugin = config.plugins.find((plugin) => plugin.constructor.name === 'ReloadPlugin' && plugin.options?.runsInNextJs);
+
+      // entryOptions requires @prefresh/webpack 4.0.2+
+      if (prefreshPlugin && prefreshPlugin.options.hasOwnProperty('entryOptions')) {
+        prefreshPlugin.options.entryOptions = {
+          ...prefreshPlugin.options.entryOptions,
+          layer: WEBPACK_LAYERS.pagesDirBrowser
+        };
+      } else {
+        config.plugins.unshift({
+          apply(compiler) {
+            compiler.hooks.thisCompilation.tap('Prefresh', (compilation) => {
+              // !IMPORTANT: This changes every entry, upgrade to @prefresh/webpack 4.0.2 to apply the above behavior
+              compilation.globalEntry.options.layer = WEBPACK_LAYERS.pagesDirBrowser;
+            });
+          }
+        });
+      }
     }
     return config;
   }
